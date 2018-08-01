bfe224d512569249915efdc23e228653f1a166a8
= <=>
2018-08-01 15:46:53 +0300

    Adding Country

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { BookingAmenityModule } from './amenity/amenity.module';
+import { BookingCountryModule } from './country/country.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
         BookingAmenityModule,
+        BookingCountryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
